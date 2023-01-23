5c55661b75e650c9076c6434ff60b1b51a193eb2
alan-viraj <avjoachim484@gmail.com>
2023-01-23 14:00:50 +0530

    Update vue.config.js

--- app/vue.config.js
+++ app/vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
             provider: "github",
             private: false,
             owner: "louismorgner",
-            repo: "jamie-release"
+            repo: "jamie-test"
           }
         ],
         dmg: {
